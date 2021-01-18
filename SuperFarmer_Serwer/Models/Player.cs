2e08d045ff51e45b369e0fc7fef40a6a160b6a56
handalphus <roman.kluger97@gmail.com>
2021-01-18 19:45:06 +0100

    Add project files.

---
+++ SuperFarmer_Serwer/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperFarmer_Serwer.Models
+{
+    public class Player
+    {
+        
+        public long Id { get; set; }
+        public string PlayerName { get; set; }
+        public int NumberOfRabbit { get; set; }
+        public int NumberOfSheep { get; set; }
+        public int NumberOfPig { get; set; }
+        public int NumberOfCow { get; set; }
+        public int NumberOfHorse { get; set; }
+        public int NumberOfDog { get; set; }
+        public int NumberOfBigDog { get; set; }
+    }
+}
